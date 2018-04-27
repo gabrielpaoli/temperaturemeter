3712d3b41c15cd212f177148165df29326f28ee7
truelogic <gpaoli@truelogic.software>
2018-04-27 11:05:17 -0300

    fix dispositivos

--- sites/all/modules/custom/temperaturemeter/js/temperaturemeter.get_temperature.js
+++ sites/all/modules/custom/temperaturemeter/js/temperaturemeter.get_temperature.js
@@ -1,38 +1,39 @@
-function callSensorData(idEmpresa = null) {
-	jQuery.ajax({
-    url: 'http://temperaturemeter.com/temperaturemeter/get/temperature',
-    type: 'POST',
-    data: jQuery.param({idEmpresa : idEmpresa}) ,
-    success: function (data) {
-			jQuery( ".sensor1" ).removeClass("temperature_color_green temperature_color_yellow temperature_color_red").addClass( data[0]['color1'] );
-			jQuery( ".sensor2" ).removeClass("temperature_color_green temperature_color_yellow temperature_color_red").addClass( data[0]['color2'] );
-			jQuery( ".sensor3" ).removeClass("temperature_color_green temperature_color_yellow temperature_color_red").addClass( data[0]['color3'] );
-			jQuery( ".sensor4" ).removeClass("temperature_color_green temperature_color_yellow temperature_color_red").addClass( data[0]['color4'] );
-
-		  jQuery( '.sensor1' ).html('Sensor 1: ' + data[0]['sensor1'] + ' C' );
-		  jQuery( '.sensor2' ).html('Sensor 2: ' + data[0]['sensor2'] + ' C' );
-		  jQuery( '.sensor3' ).html('Sensor 3: ' + data[0]['sensor3'] + ' C' );
-		  jQuery( '.sensor4' ).html('Sensor 4: ' + data[0]['sensor4'] + ' C' );
-
-			hideMessage();
-			showSensorHideLoaderGif();
-
-    },
-    error: function () {
-    	showMessage('seleccionar');
-    }
-	}); 
-}
-
 function callDispositivosData(idEmpresa = null) {
 	jQuery.ajax({
     url: 'http://temperaturemeter.com/temperaturemeter/get/dispositivosPerEmpresa',
     type: 'POST',
     data: jQuery.param({idEmpresa : idEmpresa}) ,
     success: function (data) {
-    	//console.log(data);
+
     	jQuery('.containerDispositivos').empty();
-    	jQuery('.containerDispositivos').append('<div class="sasa">hola</div>');
+
+    	jQuery.each(data, function( index, value ) {
+
+    		var nombreDelDispositivo = '<div class="nombreDelDispositivo">' + value["temperatura"]["nombre_dispositivo"] + '</div>';
+    		var sensor1 = '<div class="nombreDelDispositivo sensor1-'+index+'">Sensor 1: ' + value["temperatura"]["sensor1"] + ' C</div>';
+    		var sensor2 = '<div class="nombreDelDispositivo sensor2-'+index+'">Sensor 2: ' + value["temperatura"]["sensor2"] + ' C</div>';
+    		var sensor3 = '<div class="nombreDelDispositivo sensor3-'+index+'">Sensor 3: ' + value["temperatura"]["sensor3"] + ' C</div>';
+    		var sensor4 = '<div class="nombreDelDispositivo sensor4-'+index+'">Sensor 4: ' + value["temperatura"]["sensor4"] + ' C</div>';
+
+    		jQuery( ".containerDispositivos" ).append('<div class="containerInnDispositivos">' + 
+    			nombreDelDispositivo + 
+    			sensor1 + 
+    			sensor2 + 
+    			sensor3 + 
+    			sensor4 + 
+
+    		'</div>' );
+
+
+				jQuery( ".sensor1-"+index).removeClass("temperature_color_green temperature_color_yellow temperature_color_red").addClass( value["temperatura"]['color1'] );
+				jQuery( ".sensor2-"+index).removeClass("temperature_color_green temperature_color_yellow temperature_color_red").addClass( value["temperatura"]['color2'] );
+				jQuery( ".sensor3-"+index).removeClass("temperature_color_green temperature_color_yellow temperature_color_red").addClass( value["temperatura"]['color3'] );
+				jQuery( ".sensor4-"+index).removeClass("temperature_color_green temperature_color_yellow temperature_color_red").addClass( value["temperatura"]['color4'] );
+
+
+			  console.log( index + ": " + value["temperatura"]["nombre_dispositivo"] );
+			});
+    	
 
 			hideMessage();
 			showSensorHideLoaderGif();    	
@@ -73,17 +74,17 @@ function onChangeSelectEmpresa(){
 
 function intervalSensorData(idEmpresa) {
   temperature = setInterval( function() { 
-  	callSensorData(idEmpresa); }, 2000 
+  	callDispositivosData(idEmpresa); }, 2000 
   );
 }
 
 function showLoaderGifHideSensor(){
 	jQuery('.loader').show();
-	jQuery('.containerSensor').hide();
+	jQuery('.containerDispositivos').hide();
 }
 
 function showSensorHideLoaderGif(){
-	jQuery('.containerSensor').show();
+	jQuery('.containerDispositivos').show();
 	jQuery('.loader').hide();
 }
 
